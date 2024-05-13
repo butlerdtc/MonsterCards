8d2e9c41b2e5b9d9dcd6da3999e63d4b147d5c43
butlerdtc <butlerr2@middleton.school.nz>
2024-05-13 20:09:20 +1200

    Updated 03 v1 and created 03 v2

--- 03_add_card_v1.py
+++ 03_add_card_v1.py
@@ -1,15 +1,17 @@
 """V1 of add card component
 Asks the user to enter new card name and a value for each of the four
-categories then prints the result
+categories then prints the result. Basic structure to build on.
 Created by Robson Butler - 13/05/24
 """
 
+# These input statements ask user for new cards name and stat values
 new_name = input("Please enter the name of the new card: ").title()
 new_strength = int(input("Enter new cards strength value: "))
 new_speed = int(input("Enter new cards speed value: "))
 new_stealth = int(input("Enter new cards stealth value: "))
 new_cunning = int(input("Enter new cards cunning value: "))
 
+# These print the new card's name and stat values
 print(f"\n{new_name}:")
 print(f"Strength: {new_strength}")
 print(f"Speed: {new_speed}")
---
+++ 03_add_card_v2.py
@@ -0,0 +1,53 @@
+"""V2 of add card component
+Changes V1 to check if card is already in the catalogue and will make user
+enter new name if it is found. Now uses a list of each stat to iterate through
+instead of asking for each stat in a separate line of code.
+Created by Robson Butler - 13/05/24
+"""
+
+# Card catalogue from 00_monster_base_v1
+card_catalogue = {"Stoneling": {"Strength": 7, "Speed": 1, "Stealth": 25,
+                                "Cunning": 15},
+                  "Vexscream": {"Strength": 1, "Speed": 6, "Stealth": 21,
+                                "Cunning": 19},
+                  "Dawnmirage": {"Strength": 5, "Speed": 15, "Stealth": 18,
+                                 "Cunning": 22},
+                  "Blazegolem": {"Strength": 15, "Speed": 20, "Stealth": 23,
+                                 "Cunning": 6},
+                  "Websnake": {"Strength": 7, "Speed": 15, "Stealth": 10,
+                               "Cunning": 5},
+                  "Moldvine": {"Strength": 21, "Speed": 18, "Stealth": 14,
+                               "Cunning": 5},
+                  "Vortexwing": {"Strength": 19, "Speed": 13, "Stealth": 19,
+                                 "Cunning": 2},
+                  "Rotthing": {"Strength": 16, "Speed": 7, "Stealth": 4,
+                               "Cunning": 12},
+                  "Froststep": {"Strength": 14, "Speed": 14, "Stealth": 17,
+                                "Cunning": 4},
+                  "Wispghoul": {"Strength": 17, "Speed": 19, "Stealth": 3,
+                                "Cunning": 2}
+                  }
+
+# Stores list of stats to make iterating through each card easier
+stats = ["Strength", "Speed", "Stealth", "Cunning"]
+
+while True:
+    # Dictionary to store entered data until its confirmed and added
+    temporary_dict = {}
+    # Asks for new card name
+    new_card_name = input("Please enter name of the new card: ").title()
+    # Checks if card name is already in the catalogue
+    if new_card_name in card_catalogue:
+        print("This card is already in the catalogue\nPlease enter a new name")
+        continue
+    else:
+        # Adds new card name to temporary dictionary
+        temporary_dict[new_card_name] = {}
+        break
+
+# Iterates through each stat from the list to get new value
+for stat in stats:
+    temporary_dict[new_card_name][stat] = int(input(f"Enter {new_card_name}'s "
+                                                    f"{stat} value: "))
+# Prints new card and details
+print(temporary_dict)
