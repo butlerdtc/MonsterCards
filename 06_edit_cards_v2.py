e52d627c2b79b14cd957619133590ca1f3325a40
butlerdtc <butlerr2@middleton.school.nz>
2024-05-22 19:12:12 +1200

    Update 06_edit_cards_v2.py

--- 06_edit_cards_v2.py
+++ 06_edit_cards_v2.py
@@ -25,6 +25,8 @@ card_catalogue = {"Stoneling": {"Strength": 7, "Speed": 1, "Stealth": 25,
                   "Wispghoul": {"Strength": 17, "Speed": 19, "Stealth": 3,
                                 "Cunning": 2}
                   }
+# Stat list from 00_monster_base_v3
+stats = ["Strength", "Speed", "Stealth", "Cunning"]
 
 # Loops until user cancels or confirms the card
 while True:
@@ -42,29 +44,49 @@ while True:
     else:
         # If edit is chosen loops until user edits card or cancels
         while True:
-            chosen_card = (input("Please choose card from catalogue to edit"
-                                 "(or cancel): ").title())
+            # Makes a list of each card name to be used as temporary buttons
+            cards = []
+            for card in card_catalogue:
+                cards.append(card)
+            cards.append("Cancel")
+            # This step won't be here when broken down as other components
+            # do this step
+            chosen_card = easygui.buttonbox("Please choose card",
+                                            "Choose card",
+                                            cards)
             if chosen_card == "Cancel":
                 # Ends loop if user cancels
-                print("Cancelled")
+                easygui.msgbox("Cancelled", "Cancelled")
                 break
             if chosen_card in card_catalogue:
-                edit_name = input("Would you like to edit the card name?(Y/N)"
-                                  "").lower()
-                if edit_name == "n":
-                    searched_stat = input("What stat would you like to change?"
-                                          "").title()
-                    for name, stats in card_catalogue:
-                        if searched_stat in stats:
-                            new_value = int(input(f"Enter new value for "
-                                                  f"{searched_stat}: "))
+                # Asks if they want to change the card name
+                edit_name = easygui.buttonbox("Would you like to edit the"
+                                              " card name?", "Edit",
+                                              ["Yes", "No"])
+                if edit_name == "No":
+                    searched_stat = easygui.buttonbox("What stat would "
+                                                      "you like to change?",
+                                                      "Stats", stats)
+                    # Asks for new stat value
+                    while True:
+                        new_value = easygui.integerbox(f"Enter new value for "
+                                                       f"{searched_stat}",
+                                                       "New stat",
+                                                       upperbound=10000000,
+                                                       lowerbound=-10000000)
+                        if 1 <= new_value <= 25:
+                            # Updates stat in catalogue
                             card_catalogue[chosen_card][searched_stat] = (
                                 new_value)
                             break
                         else:
-                            print("Please enter valid stat")
-                elif edit_name == "y":
-                    new_name = input("Enter new card name: ")
+                            easygui.msgbox("Please enter a value between "
+                                           "1 and 25", "Error")
+                    break
+                else:
+                    # Asks for new name then updates it
+                    new_name = easygui.enterbox("Enter new card name",
+                                                "New name")
                     card_catalogue[new_name] = card_catalogue.pop(chosen_card)
                     break
 print(card_catalogue)
